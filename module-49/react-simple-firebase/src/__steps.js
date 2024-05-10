fdc3ae3831a453c546533c238153418e99c06a10
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-05-10 17:04:53 +0600

    update

---
+++ module-49/react-simple-firebase/src/__steps.js
@@ -0,0 +1,8 @@
+/**
+ * 1. visit: console.firebase.google.com
+ * 2. creat project(skip google analytics)
+ * 3. register app (create config)
+ * 4. install firebase
+ * 5. add config file to your project
+ * 6. Danger:  never pulish or make public "firebase.init.config" file
+ */
---
+++ module-49/react-simple-firebase/src/firebase/firebas.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaTXZMkZdwOOtcF7yYw6aEIZdbrdKjLXE",
+  authDomain: "react-simple-firebase-6311e.firebaseapp.com",
+  projectId: "react-simple-firebase-6311e",
+  storageBucket: "react-simple-firebase-6311e.appspot.com",
+  messagingSenderId: "774907384369",
+  appId: "1:774907384369:web:c25aa0cd00dd2f4301fb10"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
