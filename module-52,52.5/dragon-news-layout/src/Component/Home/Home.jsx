99a42d875077c2413f5b6838ce8a245eb7d052a5
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-05-25 07:29:41 +0600

    up

---
+++ module-52,52.5/dragon-news-layout/src/Component/Home/Home.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'; // ES6
+
+const Home = () => {
+    return (
+        <div>
+            <p>home</p>
+        </div>
+    );
+};
+
+export default Home;
+
+Home.propTypes = {
+   props : PropTypes.node
+}
--- module-52,52.5/dragon-news-layout/src/main.jsx
+++ module-52,52.5/dragon-news-layout/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './Root.jsx'
+import Home from './Component/Home/Home.jsx'
+import Error from './Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Root></Root>,
+    errorElement:<Error></Error>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+   <RouterProvider router={router}/>
+
   </React.StrictMode>,
 )
