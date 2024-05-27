efb0c7af8f11843d5f19660e7d4915e6352438db
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-05-27 21:26:12 +0600

    update all

--- module-52,52.5/dragon-news-layout/src/Pages/Shared/RightSideNavbar/RightSideNavbar.jsx
+++ module-52,52.5/dragon-news-layout/src/Pages/Shared/RightSideNavbar/RightSideNavbar.jsx
@@ -1,9 +1,63 @@
-
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FaGoogle,FaGithub, FaInstagram, FaFacebook } from "react-icons/fa";
+import auth from "../../../firebase.config";
+import { FaXTwitter } from "react-icons/fa6";
 
 const RightSideNavbar = () => {
+
+    const gitHubProvider = new GithubAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleGoogleLogIn =()=>{
+        signInWithPopup(auth, googleProvider)
+        .then(result =>{
+            console.log(result.user);
+        })
+        .catch(error =>{
+            console.log(error.message);
+        })
+    }
+
+    const handleGitHubLogIn = () =>{
+        signInWithPopup(auth, gitHubProvider)
+        .then(result =>{
+            console.log(result.user);
+        })
+        .catch(error =>{
+            console.log(error.message);
+        })
+    }
     return (
         <div>
-            
+            <div className="flex flex-col space-y-3">
+            <h3 className="text-xl font-bold text-[#403F3F] text-center lg:text-left">LogIn With</h3>
+            <button onClick={handleGoogleLogIn} className="btn bg-transparent border-2 border-[#403F3F]">
+                <FaGoogle className="text-xl"></FaGoogle>
+             LogIn with Google
+             
+            </button>
+            <button onClick={handleGitHubLogIn} className="btn bg-transparent border-2 border-[#403F3F]">
+                <FaGithub className="text-xl"></FaGithub>
+             LogIn with GitHub
+             
+            </button>
+        </div>
+
+        <div className="flex flex-col mt-6 ">
+            <h3 className="my-5 text-xl font-bold text-[#403F3F] text-center lg:text-left">Find Us On</h3>
+            <button className="flex items-center gap-2 justify-center border-2 border-[#403F3F] p-4 lg:justify-start">
+                <FaFacebook className="text-xl"></FaFacebook>
+                FaceBook
+            </button>
+            <button className="flex items-center gap-2 justify-center border-2 border-[#403F3F] p-4 lg:justify-start">
+                <FaXTwitter className="text-xl"></FaXTwitter>
+                Twitter
+            </button>
+            <button className="flex items-center gap-2 justify-center border-2 border-[#403F3F] p-4 lg:justify-start">
+                <FaInstagram className="text-xl"></FaInstagram>
+                instagram
+            </button>
+        </div>
         </div>
     );
 };
